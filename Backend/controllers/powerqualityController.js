c96fb46374db9adc1f001165053e1f10da211026
SL0023-SenseLive <SL09.senselive@outlook.com>
2025-05-28 11:12:46 +0530

    changes

--- Backend/controllers/powerqualityController.js
+++ Backend/controllers/powerqualityController.js
@@ -602,6 +602,7 @@ async function generatePowerQualityPdfFromEjs(templateData, deviceId, startDate,
 
       // Launch Puppeteer
       browser = await puppeteer.launch({
+         executablePath: '/usr/bin/chromium-browser',
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
       });
